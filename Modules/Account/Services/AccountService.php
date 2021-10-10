85b851665e201a7f994fda7798e0a4c5a2a553d4
Phelipe Momessso <phelipe.momesso@gmail.com>
2021-10-09 23:17:43 -0300

    Add features into Services

--- Modules/Account/Services/AccountService.php
+++ Modules/Account/Services/AccountService.php
@@ -2,16 +2,45 @@
 
 namespace Modules\Account\Services;
 
+use App\Models\User;
 use App\Service\BaseService;
 use Illuminate\Support\Arr;
+use Modules\Account\Entities\Account;
 use Modules\Account\Repositories\AccountRepository;
+use function PHPUnit\Framework\throwException;
 
 class AccountService extends BaseService
 {
+    const DEBIT = 'D';
+    const CREDIT = 'C';
+
     /**
      * The repository instance.
      *
      * @var AccountRepository
      */
     protected $repository = AccountRepository::class;
+
+    public function hasAccount(User $user) {
+        $account = $this->repository->findByField('user_id', $user->id);
+        if ($account->isNotEmpty())
+            return $account;
+        return false;
+    }
+
+    public function createAccount(User $user):Account {
+        return $this->repository->create([
+           'user_id' => $user->id,
+           'balance' => 0
+       ]);
+    }
+
+    public function isBalanceEnough() {
+
+    }
+
+    public function updateBalance(Account $account, float $amount, string $operation): Account {
+        $value = $operation === self::DEBIT ? $account->balance - $amount :  $account->balance + $amount;
+        return $this->repository->update(['balance'=>$value], $account->id);
+    }
 }
--- Modules/Deposit/Services/DepositService.php
+++ Modules/Deposit/Services/DepositService.php
@@ -3,7 +3,8 @@
 namespace Modules\Deposit\Services;
 
 use App\Service\BaseService;
-use Illuminate\Support\Arr;
+use Modules\Account\Entities\Account;
+use Modules\Deposit\Entities\Deposit;
 use Modules\Deposit\Repositories\DepositRepository;
 
 class DepositService extends BaseService
@@ -14,4 +15,29 @@ class DepositService extends BaseService
      * @var DepositRepository
      */
     protected $repository = DepositRepository::class;
+
+    public function makeDeposit(array $attributes, Account $account): Deposit
+    {
+        $data = [
+            'account_id' => $account->id,
+            'approved_by' => null,
+            'amount' => $attributes['amount'],
+            'approved' => 0,
+            'image' => $this->uploadCheckImage($attributes['image'])
+        ];
+        return $this->repository->create($data);
+    }
+
+    private function uploadCheckImage($image): ?string {
+        if (isset($image) && !empty($image)) {
+            $name = uniqid(date('HisYmd'));
+            $extension = $image->extension();
+            $nameFile = "{$name}.{$extension}";
+            $upload = $image->storeAs('deposits', $nameFile);
+            if (!$upload)
+                return false;
+            return $upload;
+        }
+        return null;
+    }
 }
